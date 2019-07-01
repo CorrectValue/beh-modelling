7220f3d96eb1e17b78783083f53bce46cf03d098
Lexa <maythe4bewyou@gmail.com>
2019-07-01 21:01:00 +0400

    Initial commit

---
+++ Disser/Assets/sight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sight : MonoBehaviour {
+
+    public float fieldOfViewAngle = 110f;
+    public float collectibleInSight;
+    public Vector3 lastCollectibleSighting;
+
+    private UnityEngine.AI.NavMeshAgent nav;
+    private SphereCollider col;
+    private Animator anim;
+   // private LastSighting lastSighting;
+    private GameObject collectible;
+
+    void Awake()
+    {
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        col = GetComponent<SphereCollider>();
+        anim = GetComponent<Animator>();
+       // lastSighting
+    }
+}
