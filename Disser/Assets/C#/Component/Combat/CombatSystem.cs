02896e34d0e76e9ac37cac7d047f0fcfb3bea4f2
Arsen <rasen13@mail.ru>
2019-07-12 22:39:30 +0400

    Full Project Commit
    Извините, что так долго, проект по 30 раз переделывался, каждый день как вставали, каждый час, поэтому мы думали. что нет смысла пушить, ибо через какое то время функция могла очень сильно измениться, вплоть до добавления новых функций, проект не до конца отредоктирован, сегодня прям под ночь и утро мы попытаемся решить все его проблемы, если не сложно, было бы неплохо скинуть вам отчет в ворд виде на почту.

---
+++ Disser/Assets/C#/Component/Combat/CombatSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatSystem : MonoBehaviour
+{
+    private HealthStats HS;
+    private float Damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HS = GetComponent<HealthStats>();
+    }
+
+    public bool Attack(HealthStats hs)
+    {
+        Damage = HS.HowDamage();
+        hs.CalculateHealth(Damage);
+        return true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Disser/Assets/C#/Component/NavigationSystem/NavigationSystem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationSystem : MonoBehaviour
+{
+    
+    public float    minX,   minZ;           // Минимальное значение для оси Z и X
+    public float    maxX,   maxZ;           // Максимальное значение для оси Z и X
+    private Vector3 Point;                  //Точка для следования
+    private Vector3 Position;               //Текущая позиция
+    private NavMeshAgent agent;             //Ссылка на NavMeshAgent
+    private Interaction IS;                 //
+    private bool Stop;
+    void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            newPoint();
+            Point.x = 0;
+            Point.z = 0;
+            Stop = false;
+        }                      //
+    public void SetStop(bool s){Stop = s;}
+    
+    public void newPoint()
+        {
+            if(!Stop)
+            {
+            Point.x = Random.Range(minX, maxX); // Рандомная позиция X
+            Point.z = Random.Range(minZ, maxZ); // Рандомная позиция Z
+            Debug.Log("newPoint");
+            }
+
+        }
+
+    public void SetPoint(Vector3 ActorLocation)
+        {
+            Point.x = ActorLocation.x; //Установка новой координаты X
+            Point.z = ActorLocation.z; //Установка новой координаты Z
+            Stop = true;
+            Debug.Log("newPointOK!");
+        }
+
+    bool getDistance() //Дистанция от персонажа до точки
+        {
+            double dis = Mathf.Sqrt(Mathf.Abs((Point.x - transform.position.x) + 
+                                    (Point.z - transform.position.z)));    //Математическая формула пути (От точки до точки)
+            if(dis < 1 ){Stop =  false; return true; }
+            return false;
+        }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+        {   
+            agent.SetDestination(Point);    //Следовать до точки
+            if(getDistance())             //Если врадиусе точки
+            {
+                newPoint();                 //Новая точка
+                Stop =  false;         
+            }
+        }
+}
